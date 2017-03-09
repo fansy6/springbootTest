935ca89ad59d4583840c13e12806888466010f4e
wen_li <wen_li>
2017-03-09 17:46:59 +0800

    add test

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @EnableAutoConfiguration
 public class SampleController {
 
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     @ResponseBody
     String home() {
         return "Hello World!";
--- src/test/java/Init.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Administrator on 2017/3/9.
- */
-public class Init {
-}
